6b3709838ff561701ae2b74d72a518b8321b270e
JeJn <jejn@JeJnui-MacBookAir.local>
2020-10-23 03:39:53 +0900

    #중간 합병

--- src/container/ConferenceContainer.js
+++ src/container/ConferenceContainer.js
@@ -10,19 +10,6 @@ export default () => {
 
     return (
         <section id="main_container">
-<<<<<<< HEAD
-        <div class="tab">
-            <ul>
-                <li><Link to="#!" class="on">컨퍼런스</Link></li>
-            </ul>
-        </div>
-        <div class="main_content">
-            <div class="speech">
-                <i></i>
-                <img src={require("../static/img/bg_speech.png")} alt="" />
-                <a href="#!"><img src={require("../static/img/bt_moviego.png")} alt="" /></a>
-                <span> {'<'}이름{'('}소속{')>'} </span>
-=======
             <div class="tab">
                 <ul>
                     <li><Link to="#!" class="on">{language === 'ko' ? "컨퍼런스" : "Conference"}</Link></li>
@@ -35,7 +22,6 @@ export default () => {
                     <a href=""><img src={require("../static/img/bt_moviego.png")} alt="" /></a>
                     {/* <span> {'<'}이름{'('}소속{')>'} </span> */}
                 </div>
->>>>>>> 15fe0c565f4b9f1b7dcfb684a3cb7ce8f2c5f1ab
             </div>
         </section>
     )
