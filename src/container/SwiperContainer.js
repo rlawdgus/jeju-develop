be84a158015d45945a018be17735da70531344cb
JeJn <jejn@JeJnui-MacBookAir.local>
2020-10-28 19:13:52 +0900

    Swiper 수정

--- src/container/SwiperContainer.js
+++ src/container/SwiperContainer.js
@@ -43,7 +43,7 @@ export default ({ dataSet }) => {
     //--------------------------------------------------------------------------------------
 
     return (
-        <>
+        <>{console.log("스와이퍼 입장이요")}
             {dataSet === 'Error' ? (
                 <Swiper>
                     <SwiperSlide>
@@ -51,31 +51,33 @@ export default ({ dataSet }) => {
                             className={"error" + current_pack.css}
                             src={ErrorImage}
                             alt=""
-                        />{' '}
+                        />
                     </SwiperSlide>
                 </Swiper>
             ) : (
-                    <Swiper
-                        slidesPerView={5} // 보이는 슬라이드 수
-                        slidesPerGroup={5} // 슬라이드 할때 몇개를 슬라이드 할것이냐
-                        loop={dataSet.length > 5 ? true : false}
-                        loopFillGroupWithBlank={true} // 빈공간은 빈 슬라이드로 채움
-                        loopedSlides={5} // 루프를 하면 몇개를 할것인지
-                        initslide={0}
-                        navigation
-                        watchOverflow={true}
-                        loopPreventsSlide={true} // 활성화되면 전환이 이미 진행 중일 때 스 와이퍼 슬라이드 이전 / 다음 전환을 방지
-                    >
-                        {' '}
-                        {dataSet.map((data) => (
-                            <SwiperSlide key={data.id}>
-                                <div>
-                                    <em>{data.title}</em>
-                                    <img src={URL + data.photo_1} alt="no_image" />
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
+                    <>
+                        {console.log(dataSet.length)}
+                        <Swiper
+                            slidesPerView={3} // 보이는 슬라이드 수
+                            slidesPerGroup={3} // 슬라이드 할때 몇개를 슬라이드 할것이냐
+                            loop={dataSet.length > 3 ? true : false}
+                            loopFillGroupWithBlank={true} // 빈공간은 빈 슬라이드로 채움
+                            loopedSlides={3} // 루프를 하면 몇개를 할것인지
+                            initialSlide={0}
+                            navigation
+                            watchOverflow={true} // 슬라이드가 충분하지 않은 경우 탐색 버튼을 숨김
+                            loopPreventsSlide // 활성화되면 전환이 이미 진행 중일 때 스 와이퍼 슬라이드 이전 / 다음 전환을 방지
+                        >
+                            {dataSet.map((data) => (
+                                <SwiperSlide key={data.id}>
+                                    <div>
+                                        <em>{data.title}</em>
+                                        <img src={URL + data.photo_1} alt="no_image" />
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </>
                 )}
         </>
     );
