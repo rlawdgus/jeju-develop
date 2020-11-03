64c728e448cda6c1972f96f4fd73481b416e53b9
박철현 <parkcheolhyun@cheolmaegi.local>
2020-11-03 18:31:24 +0900

    # 폴리필 재 시도

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
