15e233886c1539d7547ec8eb8e990df0221efdbf
JeongHyun <k557330@naver.com>
2020-11-03 21:25:05 +0900

    다운로드 수정

--- src/container/NoticeViewContainer.js
+++ src/container/NoticeViewContainer.js
@@ -101,14 +101,14 @@ export default ({ viewId, near }) => {
                     <div className={"viewcontent" + current_pack.css}>
                         {noticeView.contents}
                         <div className={"file" + current_pack.css}>
-                            {noticeView.file_1 && <a href={URL + noticeView.file_1} download>
+                            {noticeView.file_1 && <a href={URL + noticeView.file_1} target="_blank" download>
                                 <img
                                     src={`${process.env.PUBLIC_URL}/img/ic_download.png`}
                                     alt="download"
                                 />
                             </a>}
                             <span>
-                                <a href={URL + noticeView.file_1} download>{noticeView.file_1}</a>
+                                {noticeView.file_1 && <a href={URL + noticeView.file_1} target="_blank" download>{noticeView.file_1}</a>}
                                 <em>334kb</em>
                             </span>
                         </div>
