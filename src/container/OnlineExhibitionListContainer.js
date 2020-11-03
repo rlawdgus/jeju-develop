912eeb123d1438b89c363c08e7c1b219f05d5823
JeongHyun <k557330@naver.com>
2020-11-03 18:00:50 +0900

    스와이퍼 빈 공간, 카테고리 순서 처리

--- src/container/OnlineExhibitionListContainer.js
+++ src/container/OnlineExhibitionListContainer.js
@@ -80,6 +80,14 @@ const OnlineExhibitionListContainer = ({ type, items, loading, swiper, firstOpen
         },
         {
             num: 8,
+            id: "c11",
+            kr_text: "유제품",
+            en_text: "Dairy products",
+            cn_text: "중국어",
+            jp_text: "일본어"
+        },
+        {
+            num: 9,
             id: "c9",
             kr_text: "천연염색",
             en_text: "Dyed products",
@@ -87,18 +95,10 @@ const OnlineExhibitionListContainer = ({ type, items, loading, swiper, firstOpen
             jp_text: "일본어"
         },
         {
-            num: 9,
+            num: 10,
             id: "c5",
             kr_text: "마을공동체",
             en_text: "Local community",
-            cn_text: "중국어",
-            jp_text: "일본어"
-        },
-        {
-            num: 10,
-            id: "c11",
-            kr_text: "유제품",
-            en_text: "Dairy products",
             cn_text: "중국어",
             jp_text: "일본어"
         }
